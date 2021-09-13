a6578e2d77932b9f466c990440fac62364abba87
luketunnicliffe <luketunnicliffe@gmail.com>
2021-09-13 22:37:22 +0900

    Added CSS code

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
